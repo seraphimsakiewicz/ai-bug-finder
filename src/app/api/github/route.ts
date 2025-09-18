c047c4a43a65157f7f0cfcb6b0fe7b18ec42939c
Seraphim <seraphim.codes@gmail.com>
2025-09-18 12:47:47 -0400

    Linked input to api route to log the name of repo

--- src/app/api/github/route.ts
+++ src/app/api/github/route.ts
@@ -1,13 +1,17 @@
 import { Octokit } from "@octokit/rest";
 
-export async function GET() {
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const repoUrl = new URL(searchParams.get("repoUrl") || "");
+  const [, owner, repo] = repoUrl.pathname.split("/");
+
   const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
   });
 
   const response = await octokit.rest.repos.get({
-    owner: "octocat",
-    repo: "Hello-World",
+    owner: owner,
+    repo: repo,
   });
 
   console.log(response.data.name);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [repoName, setRepoName] = useState<string>("");
+
   const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formEl = e.currentTarget;
     const formData = new FormData(formEl);
     const repoUrl = formData.get("repoUrl");
-    const response = await fetch("/api/github");
-    await response.json();
-    formEl.reset();
+    if (typeof repoUrl === "string") {
+      const response = await fetch(
+        `/api/github?repoUrl=${encodeURIComponent(repoUrl)}`
+      );
+      const data = await response.json();
+      setRepoName(data.name);
+      formEl.reset();
+    }
   };
   return (
     <div>
@@ -18,6 +25,7 @@ export default function Home() {
         <input type="text" name="repoUrl" placeholder="GitHub repo URL" />
         <button type="submit">Analyze</button>
       </form>
+      {repoName && <p>Repo: {repoName}</p>}
     </div>
   );
 }
