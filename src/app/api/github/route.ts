88d1c9a75455a9346dc667ec140e16ff006b586b
Seraphim <seraphim.codes@gmail.com>
2025-09-18 12:01:43 -0400

    Added Octokit and did hello world

--- src/app/api/github/route.ts
+++ src/app/api/github/route.ts
@@ -1,3 +1,15 @@
-export async function GET(request: Request) {
-  return Response.json({ message: "Hello world" });
+import { Octokit } from "@octokit/rest";
+
+export async function GET() {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
+  const response = await octokit.rest.repos.get({
+    owner: "octocat",
+    repo: "Hello-World",
+  });
+
+  console.log(response.data.name);
+  return Response.json({ name: response.data.name });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+
+import React from "react";
+
 export default function Home() {
+  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formEl = e.currentTarget;
+    const formData = new FormData(formEl);
+    const repoUrl = formData.get("repoUrl");
+    const response = await fetch("/api/github");
+    await response.json();
+    formEl.reset();
+  };
   return (
     <div>
-      <input type="text" placeholder="GitHub repo URL" />
-      <button>Analyze</button>
+      <form onSubmit={submitForm}>
+        <input type="text" name="repoUrl" placeholder="GitHub repo URL" />
+        <button type="submit">Analyze</button>
+      </form>
     </div>
   );
 }
